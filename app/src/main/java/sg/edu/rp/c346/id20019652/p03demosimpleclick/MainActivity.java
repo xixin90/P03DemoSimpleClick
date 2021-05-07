cf113d98d88ba9a9228981077eb9573a57a05dcc
Liew Xi Xin <20019652@myrp.edu.sg>
2021-05-07 12:06:46 +0800

    toggle button, radio button etc L03

--- app/src/main/java/sg/edu/rp/c346/id20019652/p03demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019652/p03demosimpleclick/MainActivity.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+    ToggleButton tbtn;
+    RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +26,40 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioGroupGender);
 
+        System.out.println("test onCreate");
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Code for the action
+                String strResponse = etInput.getText().toString();
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+
+                if(checkedRadioId == R.id.radioButtonGenderMale){
+                    strResponse = "He says " + strResponse;
+                }
+                else{
+                    strResponse = "She says " + strResponse;
+                }
+                tvDisplay.setText(strResponse);
 
             }
         });
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(tbtn.isChecked()) {
+                    etInput.setEnabled(true);
+                }
+                else{
+                    etInput.setEnabled(false);
+                }
+            }
+        });
+
 
     }
 }
