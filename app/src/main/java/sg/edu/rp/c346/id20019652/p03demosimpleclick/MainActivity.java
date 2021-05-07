75f8523431daadf0c81d8e35b980189e387a048c
Liew Xi Xin <20019652@myrp.edu.sg>
2021-05-07 10:18:27 +0800

    phase 1 (L02 revision)

--- app/src/main/java/sg/edu/rp/c346/id20019652/p03demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019652/p03demosimpleclick/MainActivity.java
@@ -3,12 +3,23 @@ package sg.edu.rp.c346.id20019652.p03demosimpleclick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
     }
 }
