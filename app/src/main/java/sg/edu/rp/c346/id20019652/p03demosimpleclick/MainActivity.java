355c47b790204c64ec94fa2ec92d87996e1b010d
Liew Xi Xin <20019652@myrp.edu.sg>
2021-05-07 11:06:08 +0800

    layout github L03

--- app/src/main/java/sg/edu/rp/c346/id20019652/p03demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019652/p03demosimpleclick/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20019652.p03demosimpleclick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+
+            }
+        });
+
     }
 }
